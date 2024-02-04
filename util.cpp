26151ebd7220cf2ed4b636de9ed931e566c79ace
Ernest Lu <eluindustries@gmail.com>
2024-02-04 15:41:41 -0800

    parseStringToWords works

--- util.cpp
+++ util.cpp
@@ -15,16 +15,37 @@ std::string convToLower(std::string src)
     to a set of words based on the criteria given in the assignment **/
 std::set<std::string> parseStringToWords(string rawWords)
 {
+/*For the product name, book author, and clothing brand we define a 
+keyword to be any string of 2 or more characters. 
+If such a word has punctuation it should be split at each punctuation character 
+and the resulting substrings (of 2 or more characters) should be used as keywords
+"we'll" --> "we" + "ll"
+"men's" --> "men"
+*/
+    set<string> parsedString;
+    string temp;
+    char currentChar;
 
+    // Iterate character by character until the null character is encountered
+    for (int i = 0; rawWords[i] != '\0'; i++) {
+        currentChar = rawWords[i];
+        if (currentChar == '\'' || currentChar == ' ') {
+            if (temp.size() > 1) {
+                parsedString.insert(temp);
+            }
+            temp.clear();
+        }
+        else {
+            temp.push_back(currentChar);
+        }
+    }
 
+    if (temp.size() > 1) 
+    {
+        parsedString.insert(temp);
+    }
 
-
-
-
-
-
-
-
+    return parsedString;
 }
 
 /**************************************************
