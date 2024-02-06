338ce89f3484119348dd3783d6d8de93da0bfc76
Ernest Lu <eluindustries@gmail.com>
2024-02-06 15:52:46 -0800

    completed book

---
+++ book.cpp
@@ -0,0 +1,76 @@
+#include "book.h"
+#include <cctype>
+
+// constructor
+Book::Book(const std::string category, const std::string name, double price, int qty,
+	const std::string author, const std::string ISBN) : Product(category, name, price, qty),
+	author_(author), ISBN_(ISBN)
+{
+}
+
+// destructor
+Book::~Book()
+{
+}
+
+// # Finish this code
+std::set<std::string> Book::keywords() const
+{
+	std::set<std::string> keywords;
+	/*implement 'toLower()'*/
+	keywords.insert(convertToLowercase(this->author_));
+	keywords.insert(convertToLowercase(this->ISBN_));
+	keywords.insert(convertToLowercase(this->name_));
+	return keywords;
+}
+
+std::string Book::displayString() const
+{
+	/*
+	displays:
+
+	<name>
+	Author: <author> ISBN : <isbn>
+	<price> <quantity> left.
+
+	*/
+
+	std::string bookString;
+
+	return bookString = this->name_ + "\n" +  "Author: " +
+						this->author_ + " ISBN : " + this ->ISBN_
+						+ "\n" + std::to_string(this->price_) + " " + 
+						std::to_string(this->qty_) + " left.";
+}
+
+// # TODO
+void Book::dump(std::ostream& os) const
+{
+	/*outputs:
+	product_category
+	name
+	price
+	quantity
+	category-specific-info*/
+	//os << category_ << "\n" << name_ << "\n" << price_ << "\n" << qty_ << std::endl;
+	os << "book" << std::endl;
+	os << this->name_ << std::endl;
+	os << this->price_ << std::endl;
+	os << this->qty_ << std::endl;
+	os << this->ISBN_ << std::endl;
+	os << this->author_ << std::endl;
+}
+
+/* choosing pass by copy + return instead of pass by reference so
+   that we can preserve the case of the original author + title, etc.
+*/
+std::string Book::convertToLowercase(std::string word) const
+{
+	std::string lowercaseWord = word;
+	for (int i = 0; word[i] != '\0'; i++)
+	{
+		lowercaseWord[i] = tolower(word[i]);
+	}
+
+	return lowercaseWord;
+}
---
+++ book.h
@@ -0,0 +1,43 @@
+#ifndef BOOK_H
+#define BOOK_H
+#include "product.h"
+
+class Book : Product {
+public:
+	// constructor
+    Book(const std::string category, const std::string name, double price, int qty,
+         const std::string author, const std::string ISBN);
+
+	// destructor
+    ~Book();
+
+    /**
+     * Returns the appropriate keywords that this product should be associated with
+     */
+    std::set<std::string> keywords() const;
+
+    /**
+     * Allows for a more detailed search beyond simple keywords
+     */
+    //to be implemented in a future assignment
+    //bool isMatch(std::vector<std::string>& searchTerms) const;
+
+    /**
+     * Returns a string to display the product info for hits of the search
+     */
+    std::string displayString() const;
+
+    /**
+     * Outputs the product info in the database format
+     */
+    void dump(std::ostream& os) const;
+
+    /*helper function that allows us to have a common case (lowercase) throughout the program*/
+    std::string convertToLowercase(std::string word) const;
+
+private:
+    std::string author_;
+    std::string ISBN_;
+};
+
+#endif
