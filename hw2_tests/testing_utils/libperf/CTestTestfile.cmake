a41036ea4a9d49a4c69d84fdb906f164c8c447e1
Ernest Lu <eluindustries@gmail.com>
2024-02-13 09:57:41 -0800

    troubleshooting

--- hw2_tests/testing_utils/libperf/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /work/assignments/hw2/hw2_tests/testing_utils/libperf
-# Build directory: /work/assignments/hw2/hw2_tests/testing_utils/libperf
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
